f303676b6c6324a75e61cf07850a4860722ddfeb
Jose Carlos Joao Moreno Aleman <joao.moreno.aleman@gmail.com>
2022-12-11 09:38:43 -0500

    FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonComponent } from './person/person.component';
+import { FormsComponent } from './forms/forms.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent,
+    FormsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
